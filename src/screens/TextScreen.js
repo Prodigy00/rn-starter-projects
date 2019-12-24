f2f580bc8fa56e3bda5cd4af8e802ad65a3f83e5
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 11:27:38 +0100

    added Text input to text screen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none" //for ios and android to disable native behaviour
+        autoCorrect={false} //for ios and android to disable native behaviour
+        value={name}
+        onChangeText={text => {
+          setName(text);
+        }}
+      />
+      <Text>Your name is {name}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
