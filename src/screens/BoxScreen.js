3db381e35d1878c819ad2ab2761a061f5ca989fe
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 12:32:27 +0100

    reference for align self

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
   textTwoStyle: {
     borderWidth: 3,
     borderColor: "red",
-    alignSelf: "flex-end"
+    alignSelf: "stretch"
   },
   textThreeStyle: {
     borderWidth: 3,
